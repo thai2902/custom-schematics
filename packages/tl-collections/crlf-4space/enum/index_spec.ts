10f05a9bb63db8fe2dac82885e482d1c6e741a56
Thai Tran Thong Le <thai.le@defide-ix.com>
2017-10-06 08:51:58 +0700

    init crlf 4 space

---
+++ packages/tl-collections/crlf-4space/enum/index_spec.ts
@@ -0,0 +1,34 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { SchematicTestRunner } from '@angular-devkit/schematics/test';
+import * as path from 'path';
+import { Schema as EnumOptions } from './schema';
+
+
+describe('Enum Schematic', () => {
+  const schematicRunner = new SchematicTestRunner(
+    '@schematics/angular',
+    path.join(__dirname, '../collection.json'),
+  );
+  const defaultOptions: EnumOptions = {
+    name: 'foo',
+    path: 'app',
+    sourceDir: 'src',
+  };
+
+  it('should create an enumeration', () => {
+    const options = { ...defaultOptions };
+
+    const tree = schematicRunner.runSchematic('enum', options);
+    const files = tree.files;
+    expect(files.length).toEqual(1);
+    expect(files.indexOf('/src/app/foo.enum.ts')).toBeGreaterThanOrEqual(0);
+  });
+
+
+});
